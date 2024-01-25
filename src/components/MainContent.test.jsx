c44380a033b52b38703e69644fd9bb1bff665fae
zizou49 <ziedktari@outlook.fr>
2024-01-25 18:22:44 +0100

    modify comment

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
-//add comment now
+//add comment now hello github
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
