1182679750ff2ddd127b4fe82b225c650257ba3f
zizou49 <ziedktari@outlook.fr>
2024-01-25 15:29:22 +0100

    add comment on class mainContext [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
-//add comment
+//add comment now
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
