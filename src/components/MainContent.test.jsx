a06863273ab94a4c146d212e7b41131416712bd5
zizou49 <ziedktari@outlook.fr>
2024-01-25 15:26:36 +0100

    add comment [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,6 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
+//add comment
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
