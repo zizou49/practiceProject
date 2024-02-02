26cfcfd70215a2534f77eaf18ed89f7c44adb5fe
zizou49 <ziedktari@outlook.fr>
2024-02-02 12:07:09 +0100

    add tag release

--- AutomationUI/cypress/e2e/1-getting-started/testreactapp.cy.js
+++ AutomationUI/cypress/e2e/1-getting-started/testreactapp.cy.js
@@ -1,12 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit(Cypress.env('MY_URL'));
-    // Assuming your link has a class "App-link"
-    cy.get('.App-link')
-    .should('have.attr', 'href', 'https://reactjs.org') // Check the href attribute
-    .should('have.attr', 'target', '_blank')           // Check the target attribute
-    .should('have.attr', 'rel', 'noopener noreferrer')  // Check the rel attribute
-    .should('have.text', 'Learn React');                // Check the text content
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.visit(Cypress.env('MY_URL'));
+//     // Assuming your link has a class "App-link"
+//     cy.get('.App-link')
+//     .should('have.attr', 'href', 'https://reactjs.org') // Check the href attribute
+//     .should('have.attr', 'target', '_blank')           // Check the target attribute
+//     .should('have.attr', 'rel', 'noopener noreferrer')  // Check the rel attribute
+//     .should('have.text', 'Learn React');                // Check the text content
 
-  })
-})
+//   })
+// })
